0424200120b6a806b24eb98fbd95a94d4b739a3d
Ashlin Eldridge <ashlin.eldridge@auspost.com.au>
2014-09-23 11:08:44 +1000

    Removed local Nexus repos.

--- project.clj
+++ project.clj
@@ -13,11 +13,7 @@
                        :dependencies [[org.clojure/tools.namespace "0.2.6"]]}
              :test    {:resource-paths ["test_resources"]}}
 
-  :repositories [["central" {:url "http://nexus1.npe.apdm.local/nexus/content/repositories/central/"}]
-                 ["clojars" {:url "http://nexus1.npe.apdm.local/nexus/content/repositories/clojars/"}]
-                 ["new-core" {:url "http://nexus1.npe.apdm.local/nexus/content/repositories/new-core/" :username "admin" :password "admin123" :sign-releases false}]]
-
-  :plugins [[lein-ancient "0.5.5"]]
+ß  :plugins [[lein-ancient "0.5.5"]]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [jarohen/nomad "0.7.0"]])
